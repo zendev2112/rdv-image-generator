f9fe93da3b28789d76fb04f5f0c3c1918ee7a15e
zen <zenon.deviagge@gmail.com>
2025-06-24 08:09:34 -0300

    try eraseing newspaper emoji

--- js/templates/instagram-templates.js
+++ js/templates/instagram-templates.js
@@ -205,7 +205,7 @@ function generateInstagramTitle(title, config) {
     
     // Add emoji for engagement (Instagram-specific)
     if (!optimizedTitle.match(/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/u)) {
-        optimizedTitle = `📰 ${optimizedTitle}`;
+        optimizedTitle = `${optimizedTitle}`;
     }
     
     return optimizedTitle;
